bea429bddccf6284a27850ed8dfed2cc76283199
Bryce Mayday <bmayday20@gmail.com>
2025-11-08 09:09:22 -0500

    Initial Setup

---
+++ .history/server_20251107145317.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index.js')
+
+app.set('view engine' , 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout' , 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+moongoose.connect('process.env.DATABASE_URL', {})
+
+
+
+app.use('/' , indexRouter)
+
+
+
+
+
+app.listen(process.env.PORT || 3000)
---
+++ .history/server_20251107145457.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index.js')
+
+app.set('view engine' , 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout' , 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+moongoose.connect('process.env.DATABASE_URL', {useNewURLParser: true})
+const db = mongoose.connection
+db.on('error')
+
+
+app.use('/' , indexRouter)
+
+
+
+
+
+app.listen(process.env.PORT || 3000)
